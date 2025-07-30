f825159590d568f95650a8a6ac2323b632c596b6
kailash2111111 <kailashsolanke8379@gmail.com>
2025-07-30 13:12:32 +0530

    To solve some issue power in the Power function

--- RSA/RSA_Algorithm/machine.cpp
+++ RSA/RSA_Algorithm/machine.cpp
@@ -1,4 +1,6 @@
 #include "machine.h"
+#include <iostream>
+#include <cmath>
 
 int  machine::gcdN(int a, int b) {
     while (b != 0) {
@@ -10,7 +12,7 @@ int  machine::gcdN(int a, int b) {
 }
 
 
-int machine::modInverse(int e, int phi) {
+int machine::modInverse(int &e, int &phi) {
     for (int d = 1; d < phi; d++) {
         if ((d * e) % phi == 1)
             return d;
@@ -19,17 +21,70 @@ int machine::modInverse(int e, int phi) {
 }
 
 
-int  machine::Power(int base, int exp, int mod) {
+int  machine::power(int base, int exp, int mod) {
+    std ::cout << "base : " << base << "exp : " << exp << "mod : "<< mod << std::endl;
     int result = 1;
     base = base % mod;
 
-    while (exp > 0) {
-        if (exp % 2 == 1)
-            result = (result * base) % mod;
+    while (exp != 0) {
+        if (exp & 1)
+            result = (result *1LL * base) % mod;
 
-        exp = exp / 2;  // exp /= 2
-        base = (base * base) % mod;
+        base = (base *1LL* base) % mod;
+      //  std::cout << "expo : " << exp << std::endl;
+        exp = exp /2 ;  
+
+
     }
 
     return result;
 }
+//int machine::power(int base, int exp, int mod) {
+//    int result = 1;
+//    base = base % mod;
+//
+//    while (exp != 0) {
+//        if (exp & 1) {
+//            result = (result * base) % mod;
+//        }
+//
+//        base = (base * base) % mod;
+//        exp = exp / 2;  // Or: exp >>= 1;
+//    }
+//
+//    return result;
+//}
+
+//int machine::power(int base, int expo, int m) {
+//    int res = 1;
+//    base = base % m;
+//    std::cout << " start base : " << base << std::endl;
+//
+//    while (expo != 0) {
+//        if (expo & 1) {
+//            std::cout << "in if condion";
+//            res = (res * 1LL * base) % m;
+//        }
+//
+//        std::cout << "res : "<<res << std::endl;
+//
+//        base = (base * 1LL * base) % m;
+//        
+//        std::cout << "base : " << base << std::endl;
+//
+//
+//        expo = expo / 2;
+//        std::cout << "expo : " << expo << std::endl;
+//
+//    }
+//    return res;
+//}
+
+//int  machine::power(int base, int expo, int m) {
+//
+//   long temp = pow(base, expo);
+//
+//    return  (long)fmod(temp, m);
+//}
+
+
